10a98145c2e74b40551b4d2128990f31a53ae0d1
Bensouda Amine <amine.bensouda@hotmail.com>
2021-04-15 13:24:42 +0200

    Bug fixed, forme, optimisation du temps de traitement et ré-organisation

---
+++ app_dash.py
@@ -0,0 +1,141 @@
+import base64
+import os, shutil
+from urllib.parse import quote as urlquote
+from flask import Flask, send_from_directory
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import glob
+import convert
+import time
+import dash_bootstrap_components as dbc
+
+
+#https://docs.faculty.ai/user-guide/apps/examples/dash_file_upload_download.html
+# Normally, Dash creates its own Flask server internally. By creating our own,
+# we can create a route for downloading files directly:
+server = Flask(__name__)
+app = dash.Dash(server=server)
+
+current_path = os.getcwd()
+
+vert = '#009081'
+XBRL_DIRECTORY = os.path.join(current_path, 'Entrees_XBRL')
+CSV_DIRECTORY = os.path.join(current_path, 'Sorties_CSV')
+
+#Vider CSV_DIRECTORY et XBRL_DIRECTORY
+def clean_folder(folder) : 
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+clean_folder(XBRL_DIRECTORY)
+clean_folder(CSV_DIRECTORY)
+
+@server.route("/download/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(XBRL_DIRECTORY, path, as_attachment=True)
+
+
+app.layout = dbc.Container(
+    [
+        html.Div(style={'backgroundColor': '#009081',
+                        'height' : '20px' },),
+        dbc.Row(
+            [
+                dbc.Col(html.Img(src=app.get_asset_url('logo.png'),
+                                 style={'height':'17%', 'width':'17%'}),
+                    width=2),
+                dbc.Col(html.H2("Convertisseur de fichiers XBRL en tableaux EXCEL"),
+                    width=4)]
+            ),
+        html.H4("Zone de dépôt XBRL :"),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(
+                ["Glisser puis déposer ou cliquer pour choisir un fichier à charger."]
+            ),
+            style={
+                "width": "50%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+                
+            },
+            multiple=True,
+            
+        ),
+        html.H4("Fichiers à télécharger :"),
+        dcc.Loading(children=[html.Ul(id="file-list")]),
+        html.Div(id="time-indic"),
+    ], 
+    fluid=True
+)
+
+
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(XBRL_DIRECTORY, name), "wb") as fp:
+        fp.write(base64.decodebytes(data))
+
+
+def uploaded_files(URL):
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(URL):
+        path = os.path.join(URL, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+@app.callback(
+    [Output("file-list", "children"), Output("time-indic", "children")],
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
+)
+def update_output(uploaded_filenames, uploaded_file_contents):
+    
+    start = time.process_time()   
+
+    if uploaded_filenames is not None and uploaded_file_contents is not None:
+        for name, data in zip(uploaded_filenames, uploaded_file_contents):
+            save_file(name, data)
+            print('Fichier sauvegardé')
+            
+    
+    xbrl_files = uploaded_files(XBRL_DIRECTORY)
+    for i in xbrl_files :
+        convert.convertir(CSV_DIRECTORY, i, XBRL_DIRECTORY)
+        print('Fichier converti')
+    
+    csv_files = uploaded_files(CSV_DIRECTORY) 
+    if len(csv_files) == 0:
+        csv_list = [html.Li("Aucun fichier n'a encore été chargé")]
+    else :
+        csv_list = [html.Li(file_download_link(filename)) for filename in csv_files]
+    
+    execution_time_sentence = "Le traitement a duré : " + str(time.process_time() - start) + " secondes."
+    
+    return csv_list, execution_time_sentence
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8080)
---
+++ convert.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import re
+import pathlib
+from bs4 import BeautifulSoup as BS  
+import glob
+import os
+from flask import Flask, render_template
+
+# August 2020
+# Parse iXBRL reports, published as .html or .xhtml file from local
+# Minimal example: Use of imports is kept to an absolute minimum 
+
+
+def convertir(mycsvpath, file_name, file_path):
+# Define path and import html or xhtml from local
+    #try:
+     #  os.mkdir('mon_csv')
+    #except:
+     #   pass
+    
+    #filepath = str(pathlib.Path.cwd())
+    #filename = glob.glob('*.xhtml')
+    print(file_name)
+    print(file_path)
+    Mypath = file_path+"/"+file_name
+    
+    print(Mypath)
+    # To read from online source just change this part
+    soup = BS(open(Mypath, "rb"), "html.parser")
+    #soup = BS(open(mypath+"evonik.xhtml", "rb"), "html.parser")
+    
+    #take out the extension
+    file_label = file_name.split(".")[0] 
+    # Set up a file to store the result@s
+    outFile = open(mycsvpath+"/"+file_label+".csv", 'w', newline='\r', encoding='utf8')
+    
+    #Initialize a csv file
+    outFile.write("position;tag;type;Valeur_numérique;Unite;Date;identi;dim;form \n")
+    
+    
+    # Loop over elements in html/xhtml
+    position = 0
+    for c, element in enumerate(soup.find_all()):
+        # Iterate over all XBLR content (identif. by "contextref")
+        if "contextref" in element.attrs:
+            # See all relevant attributes of an XBRL tag
+            #print(element.attrs)
+    
+            #################################################
+            # Retrieve content per tag
+    
+            context_ref= element.attrs['contextref']
+            # Name of entry (aka name of XBLR tag)
+            tag = str(element.attrs['name'])
+            
+            # Tagtype (standard ifrs vs. custom)
+            if "ifrs-full" in tag:
+                tagtype="IFRS"
+            else:
+                tagtype="Custom"        
+            tag = tag[tag.rfind(":")+1:]
+            tag = re.findall('[a-zA-Z][^A-Z]*',tag)
+            for i in range (len(tag)-1):
+                tag[i+1] = tag[i+1].lower()
+                
+            tag = ' '.join(tag)
+    
+            # Get content (text in tag)
+            # This is e.g. the numeric value of a position
+            content = element.text
+            content = content.replace('\n', '').replace('\r', '').lstrip()
+            # Get unit (like USD or EUR)
+            try:
+                # Somewhere hidden at top of file
+                #  <xbrli:unit id="ibee02267a6cf4a178ffe2b57731cf158_0f1b2a3d-0585-35e4-9bf4-b4f1337c8ffb">
+                #  <xbrli:measure>iso4217:USD</xbrli:measure>
+                unit = str(soup.find(id=element['unitref']).text)
+                unit = unit.strip().replace('\n', '').replace('\r', '')
+            except:
+                unit = "nn"
+            
+            # Get time and identity reference 
+            try:
+                identi="nn"
+                date1="nn"
+                date2="nn"
+                dim="nn"
+                #print(soup.find(id=element['contextref']))
+                for c in soup.find_all(id=element['contextref']):
+                    # To plain text
+                    res = c.text
+                    res = res.replace('\n', '').replace('\r', '').lstrip()
+                    # Find date by regex
+                    match = re.search(r'\d{4}-\d{2}-\d{2}', res)
+                    date1 = match.group()
+                    # Remove date from string
+                    res = res.replace(date1,"")
+                    # Find second date
+                    match = re.search(r'\d{4}-\d{2}-\d{2}', res)
+                    date2 = match.group()
+    
+                    # Remove second date
+                    res = res.replace(date2,"")
+                    # Find ifrs-ref if present
+                    dim = res[res.find("ifrs"):]
+                    if len(dim)>2:
+                        res = res.replace(dim,"")
+                    else:
+                        dim = "nn"
+                    
+    
+                        
+                    # Remove ifrs reference if prsent
+                    identi = res
+            except:
+                pass
+    
+            # Internal ID of tag
+            try:
+                cid = str(element.attrs['id'])
+            except:
+                cid = "nn"
+            
+            
+            # Number of decimals
+            try:
+                decimals = str(element.attrs['decimals'])
+            except:
+                decimals = "nn"
+    
+            # Sign of numeric value (only if negative, aka. "minus")
+            try:
+                sign = str(element.attrs['sign'])
+            except:
+                sign = ""
+    
+            # Format of numerical content (decimal separator et al.)
+            # https://www.xbrl.org/Specification/inlineXBRL-transformationRegistry/REC-2015-02-26/inlineXBRL-transformationRegistry-REC-2015-02-26.html
+            try:
+                form = str(element.attrs['format'])
+            except:
+                form = "nn"
+            
+            # Scale of numerical content
+            # E.g. when figures are expressed in "million EUR"
+            # Numerical values to be transformed by: numcontent * 10**int(scale)
+            try:
+                scale = str(element.attrs['scale'])
+            except:
+                scale = "nn"
+            
+            #################################################
+            ## Prepare content
+            content = sign + content
+            # Check if content is a number if true assign numerical value to separate variable
+            numcontent = "0"
+            try:
+                x=content.replace(",","").replace(".","").replace("-","")
+                if x.isdigit()==True: 
+                    # If number... do formating
+                    
+    
+                    # CASE: Comma decimal separatot to UK/US
+                    if form=="ixt:numcomma" or form=="ixt:numdotcomma" or form=="numspacecomma":
+                        numcontent = content.replace(".","")
+                        numcontent = content.replace(",",".")
+                    # CASE: UK/US format
+                    if form=="ixt:numcommadot" or form=="ixt:numspacedot" or form=="ixt:num-dot-decimal":
+                        numcontent = content.replace(",","")
+                    else:
+                        pass
+                    
+                    # As float
+                    numcontent = float(numcontent)
+                    # Scale (using "10 to the power of" scale)
+                    if scale!="nn":
+                        numcontent = numcontent * 10**int(scale)
+                        numcontent = str(numcontent)
+                position = position+1
+            except:
+                pass
+            
+            date3 = date2
+            if date3 == "nn":
+                date3 = date1;
+    
+            
+            # Write to csv file
+            outFile.write(str(position) +";"+ tag +";"+ tagtype +";"+ str(numcontent) +";"+ unit +";"+date3 + ";"  +identi +";"+dim +";"+form +"\n")
+    
+    outFile.close()
+
